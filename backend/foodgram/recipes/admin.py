42bff7a5ae49a4663f3a8bbb93f2ec387ba3c021
Pavel Verveyn <hive937@yandex.ru>
2023-03-15 18:22:40 +0300

    3rd review

--- backend/foodgram/recipes/admin.py
+++ backend/foodgram/recipes/admin.py
@@ -34,6 +34,7 @@ class RecipeAdmin(admin.ModelAdmin):
 class IngredientAdmin(admin.ModelAdmin):
     list_display = ('name', 'measurement_unit',)
     list_filter = ('name',)
+    search_fields = ('name',)
 
 
 @admin.register(Tag)
